4b437802472982964bf6d6f003d886d85f5faf2f
JhonatasMJ <jhonatasjhmj@gmail.com>
2025-07-14 10:54:26 -0300

    feat:end project

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,13 +1,10 @@
-import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
-import { schema } from "../../db/schema/index.ts";
-import { db } from "../../db/connection.ts";
-import { count, eq } from "drizzle-orm";
-
-
-
+import { count, eq } from 'drizzle-orm'
+import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
+import { db } from '../../db/connection.ts'
+import { schema } from '../../db/schema/index.ts'
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
-  app.get("/rooms", async () => {
+  app.get('/rooms', async () => {
     const results = await db
       .select({
         id: schema.rooms.id,
@@ -16,10 +13,10 @@ export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
         questionsCount: count(schema.questions.id),
       })
       .from(schema.rooms)
-      .leftJoin(schema.questions, eq(schema.questions.roomId ,schema.rooms.id, ))
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
       .groupBy(schema.rooms.id)
-      .orderBy(schema.rooms.createdAt);
+      .orderBy(schema.rooms.createdAt)
 
-    return results;
-  });
-};
+    return results
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,37 @@
-import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
-} from 'fastify-type-provider-zod';
-import { fastifyCors } from '@fastify/cors';
-import 'dotenv/config';
-import { env } from './env.ts';
-import { getRoomsRoute } from './http/routes/get-rooms.ts';
-import { createRoomRoute } from './http/routes/create-room.ts';
-import { getRoomQuestions } from './http/routes/get-room-questions.ts';
-import fastifyMultipart from '@fastify/multipart';
-import { createQuestionRoute } from './http/routes/create-question.ts';
-import { uploadAudioRoute } from './http/routes/upload-audio.ts';
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
-
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
-});
+})
 
 app.register(fastifyMultipart)
 
-
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-  return 'OK';
-});
+  return 'OK'
+})
 
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
 app.register(uploadAudioRoute)
-
 
 app.listen({ port: env.PORT })
